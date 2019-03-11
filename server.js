6ea280afbeb8a9f039b0fdae44ab1e4fefc8bd58
Colin <beutt102@mail.chapman.edu>
2019-03-11 12:52:27 -0700

    udpate herokubuildpacks

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-<<<<<<< HEAD
-// app.use(express.static(path.join(__dirname, 's')));
-=======
 // app.use(express.static(path.join(__dirname, 'app')));
->>>>>>> b02c4d342df52a539ee675685cebfe47aadb811d
 
 
 
@@ -21,11 +17,7 @@ app.use(express.json());
 // Define any path routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname+ "/public/index.html"));
-<<<<<<< HEAD
   // res.sendFile(path.join(__dirname+ "/src"));
-=======
-//   res.sendFile(path.join(__dirname, "/src/app.js"));
->>>>>>> b02c4d342df52a539ee675685cebfe47aadb811d
 });
 
 
