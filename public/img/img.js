18c9682a20d1506da056840f5d8fe71235daa795
Colin <beutt102@mail.chapman.edu>
2019-03-13 15:23:24 -0700

    update

---
+++ public/img/img.js
@@ -0,0 +1,6 @@
+import { builtinModules } from "module";
+
+ import avatar from "/20190302_130433 (2).jpg";
+// import bgimg from "/lego_puzzle_details_126260_444x250.jpg";
+
+modules.export ="20190302_130433 (2).jpg"
